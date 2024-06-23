b2d22e3825e129d7e8f43967b466342ac8513f3b
Petrick Ribeiro <petrick@spacecom.com.br>
2024-06-22 21:41:29 -0300

    feat: add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      black: "#2C2C2C",
+      blue: "#A4BDC2",
+      base: "#FDFEF6",
+      peach: "#FFECDC",
+      rose: "#E6DDDE",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      playwrite: ["Playwrite BR", "cursive"],
+      roboto: ["Roboto Condensed"],
+      "roboto-serif": ["Roboto Serif", "sans-serif"],
+    },
   },
   plugins: [],
 };

4a70090089b7cd5de2aea5d74f111d6d934083bd
Petrick Ribeiro <petrick@spacecom.com.br>
2024-06-25 17:15:51 -0300

    chore: add public images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="skills" />
+            <Route path="project" />
+            <Route path="about" />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;

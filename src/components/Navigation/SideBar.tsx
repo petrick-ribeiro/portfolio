a6ecae826f27866547971cba54f711d96c1e0173
Petrick Ribeiro <petrick@spacecom.com.br>
2024-07-02 17:32:38 -0300

    chore: create initial Section hook structure

--- src/components/Navigation/SideBar.tsx
+++ src/components/Navigation/SideBar.tsx
@@ -7,86 +7,85 @@ import LibraryBookSVGIcon from "../../assets/svg/LibraryBookSVGIcon";
 import { SideBarLink } from "./SideBarLink";
 import ProjectSVGIcon from "../../assets/svg/ProjectSVGIcon";
 import MenuCloseSVGIcon from "../../assets/svg/MenuCloseSVGIcon";
+import { SideBarProps } from "../types/SideBarProps";
 
-const SideBar = () => {
+const SideBar = ({ activeSection }: SideBarProps) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
-      <aside className="h-full w-fit rounded-lg flex flex-col justify-start px-4 gap-6 bg-base shadow-lg">
-        <button
-          className={`${isOpen ? "hidden" : ""} p-2`}
-          onClick={() => {
-            setIsOpen(true);
-          }}
+    <aside className="h-full w-fit rounded-lg flex flex-col justify-start px-4 gap-6 bg-base shadow-lg">
+      <button
+        className={`${isOpen ? "hidden" : ""} p-2`}
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        <MenuSVGIcon width={28} />
+      </button>
+
+      <button
+        className={`${isOpen ? "flex justify-end" : "hidden"} p-2`}
+        onClick={() => {
+          setIsOpen(false);
+        }}
+      >
+        <MenuCloseSVGIcon width={28} />
+      </button>
+
+      <div className={`rounded-lg p-2 hover:bg-blue ${activeSection === "skills" ? "bg-blue" : ""}`}>
+        <SideBarLink
+          isVisible={isOpen}
+          fill="#2C2C2C"
+          width={28}
+          info="Habilidades"
         >
-          <MenuSVGIcon width={28} />
-        </button>
+          {SkillsSVGIcon}
+        </SideBarLink>
+      </div>
 
-        <button
-          className={`${isOpen ? "flex justify-end" : "hidden"} p-2`}
-          onClick={() => {
-            setIsOpen(false);
-          }}
+      <div className={`rounded-lg p-2 hover:bg-blue ${activeSection === "project" ? "bg-blue" : ""}`}>
+        <SideBarLink
+          isVisible={isOpen}
+          fill="#2C2C2C"
+          width={28}
+          info="Projetos"
         >
-          <MenuCloseSVGIcon width={28} />
-        </button>
+          {ProjectSVGIcon}
+        </SideBarLink>
+      </div>
 
-        <div className="rounded-lg p-2 hover:bg-blue">
-          <SideBarLink
-            isVisible={isOpen}
-            fill="#2C2C2C"
-            width={28}
-            info="Habilidades"
-          >
-            {SkillsSVGIcon}
-          </SideBarLink>
-        </div>
+      <div className={`rounded-lg p-2 hover:bg-blue ${activeSection === "work" ? "bg-blue" : ""}`}>
+        <SideBarLink
+          isVisible={isOpen}
+          fill="#2C2C2C"
+          width={28}
+          info="Trabalho"
+        >
+          {WorkSVGIcon}
+        </SideBarLink>
+      </div>
 
-        <div className="rounded-lg p-2 hover:bg-blue">
-          <SideBarLink
-            isVisible={isOpen}
-            fill="#2C2C2C"
-            width={28}
-            info="Projetos"
-          >
-            {ProjectSVGIcon}
-          </SideBarLink>
-        </div>
+      <div className={`rounded-lg p-2 hover:bg-blue ${activeSection === "education" ? "bg-blue" : ""}`}>
+        <SideBarLink
+          isVisible={isOpen}
+          fill="#2C2C2C"
+          width={28}
+          info="Formação"
+        >
+          {EducationSVGIcon}
+        </SideBarLink>
+      </div>
 
-        <div className="rounded-lg p-2 hover:bg-blue">
-          <SideBarLink
-            isVisible={isOpen}
-            fill="#2C2C2C"
-            width={28}
-            info="Trabalho"
-          >
-            {WorkSVGIcon}
-          </SideBarLink>
-        </div>
-
-        <div className="rounded-lg p-2 hover:bg-blue">
-          <SideBarLink
-            isVisible={isOpen}
-            fill="#2C2C2C"
-            width={28}
-            info="Formação"
-          >
-            {EducationSVGIcon}
-          </SideBarLink>
-        </div>
-
-        <div className="rounded-lg p-2 hover:bg-blue">
-          <SideBarLink
-            isVisible={isOpen}
-            fill="#2C2C2C"
-            width={28}
-            info="Cursos"
-          >
-            {LibraryBookSVGIcon}
-          </SideBarLink>
-        </div>
-      </aside>
-    </>
+      <div className={`rounded-lg p-2 hover:bg-blue ${activeSection === "courses" ? "bg-blue" : ""}`}>
+        <SideBarLink
+          isVisible={isOpen}
+          fill="#2C2C2C"
+          width={28}
+          info="Cursos"
+        >
+          {LibraryBookSVGIcon}
+        </SideBarLink>
+      </div>
+    </aside>
   );
 };
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,42 @@
+import { useRef, useState } from "react";
 import SideBar from "../../components/Navigation/SideBar";
 import CoursesSection from "./components/CoursesSection";
 import EducationSection from "./components/EducationSection";
 import HomeCard from "./components/HomeCard";
 import SkillsSection from "./components/SkillsSection";
 import WorkSection from "./components/WorkSection";
+import useSectionOnScreen from "../../hooks/useSectionOnScreen";
 
 const Home = () => {
+  const skillsRef = useRef(null)
+  const workRef = useRef(null)
+  const educationRef = useRef(null)
+  const coursesRef = useRef(null)
+  const [activeSection, setActiveSection] = useState("")
+  const refs = [skillsRef, workRef, educationRef, coursesRef]
+
+  useSectionOnScreen({
+    refs,
+    onVisibilityChange: (visibleSections) => {
+      if (visibleSections.has(skillsRef.current)) {
+        setActiveSection("skills")
+      } else if (visibleSections.has(workRef.current)) {
+        setActiveSection("work")
+      } else if (visibleSections.has(educationRef.current)) {
+        setActiveSection("education")
+      } else if (visibleSections.has(coursesRef.current)) {
+        setActiveSection("courses")
+      } else {
+        setActiveSection("")
+      }
+    }
+  })
+
   return (
     <main className="flex flex-col min-h-screen bg-white">
       <div className="col-span-full grid grid-cols-[repeat(11,1fr)]">
         <div className="p-2 col-start-0 col-end-1">
-          <SideBar />
+          <SideBar activeSection={activeSection} />
         </div>
 
         <div className="col-span-full col-start-1 col-end-5 flex items-start justify-center rounded-lg py-20 bg-base shadow-lg">
@@ -18,13 +44,21 @@ const Home = () => {
         </div>
 
         <div className="h-screen overflow-scroll overflow-x-hidden col-start-5 col-end-12 space-y-24 py-20 px-20 bg-base">
-          <SkillsSection />
+          <div ref={skillsRef}>
+            <SkillsSection />
+          </div>
 
-          <WorkSection />
+          <div ref={workRef}>
+            <WorkSection />
+          </div>
 
-          <EducationSection />
+          <div ref={educationRef}>
+            <EducationSection />
+          </div>
 
-          <CoursesSection />
+          <div ref={coursesRef}>
+            <CoursesSection />
+          </div>
         </div>
       </div>
     </main>

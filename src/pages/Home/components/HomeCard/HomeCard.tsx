da200139533f7fe8a2bb6bf7ecd578dc2cfebb7c
Petrick <hello.petrick@gmail.com>
2024-07-12 23:21:48 -0300

    feat: add portfolio link in HomeCard

--- src/pages/Home/components/HomeCard/HomeCard.tsx
+++ src/pages/Home/components/HomeCard/HomeCard.tsx
@@ -1,6 +1,7 @@
 import GitHubSVGIcon from "../../../../assets/svg/GitHubSVGIcon";
 import GmailSVGIcon from "../../../../assets/svg/GmailSVGIcon";
 import LinkedinSVGIcon from "../../../../assets/svg/LinkedinSVGIcon";
+import NewWindowLinkSVGIcon from "../../../../assets/svg/NewWindowLinkSVGIcon";
 import Icon from "../../../../components/Icon";
 import profileImage from "/img/me.jpeg";
 import wavingGif from "/img/waving.gif";
@@ -9,7 +10,11 @@ const HomeCard = () => {
   return (
     <section className="space-y-4">
       <div className="space-y-8 font-bold text-4xl">
-        <img className="object-cover h-64 w-64 rounded-full shadow-lg" src={profileImage} alt="" />
+        <img
+          className="object-cover h-64 w-64 rounded-full shadow-lg"
+          src={profileImage}
+          alt=""
+        />
 
         <div className="space-x-2">
           <div className="flex space-x-2">
@@ -39,6 +44,10 @@ const HomeCard = () => {
 
         <Icon link="mailto:hello.petrick@gmail.com">
           <GmailSVGIcon width={28} />
+        </Icon>
+
+        <Icon link="https://github.com/petrick-ribeiro/portfolio">
+          <NewWindowLinkSVGIcon width={28} />
         </Icon>
       </div>
     </section>

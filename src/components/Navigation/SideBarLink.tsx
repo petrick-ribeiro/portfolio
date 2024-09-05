b578a32cd016d324442cdf7ba7f74e10fa61c87d
Petrick <hello.petrick@gmail.com>
2024-09-04 22:19:24 -0300

    chore: remove navbar open menu feature

--- src/components/Navigation/SideBarLink.tsx
+++ src/components/Navigation/SideBarLink.tsx
@@ -10,29 +10,20 @@ const handleScroll = (sectionID: string) => {
 export const SideBarLink = ({
   href,
   children: Icon,
-  isNavbarOpen,
   isLinkActive,
   sectionID,
   width,
   fill,
   fillHover,
-  info,
 }: SidebarLinkProps) => {
   return (
     <a
       href={href}
       className={`sidebar-button
-        ${isNavbarOpen ? " flex gap-2 items-center" : ""}
         ${isLinkActive ? " sidebar-button-activated" : ""}`}
       onClick={() => handleScroll(sectionID)}
     >
       <Icon width={width} fill={isLinkActive ? fillHover : fill} />
-
-      <span
-        className={`text-md text-center + ${isNavbarOpen ? "" : "hidden"} `}
-      >
-        {info}
-      </span>
     </a>
   );
 };
--- src/components/types/SidebarLinkProps.ts
+++ src/components/types/SidebarLinkProps.ts
@@ -3,11 +3,9 @@ import { SVGIconProps } from "../../assets/svg/types/SVGIconProps";
 export type SidebarLinkProps = {
   href?: string;
   children: React.ComponentType<SVGIconProps>;
-  isNavbarOpen: boolean;
   isLinkActive: boolean;
   sectionID: string;
   width: number;
   fill: string;
   fillHover: string;
-  info: string;
 };
